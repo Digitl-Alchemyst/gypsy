0d98d7995569beb3670a019599b5e0760fb8e933
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-06-12 20:54:31 -0500

    Login Bugged

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import GPT from '../public/GPT.png'
 
@@ -16,8 +16,8 @@ function Login() {
       />
       
       <button 
-        className="bg-slate-700 text-slate-200 font-bold text-3xl motion-safe:animate-pulse p-2 rounded-md shadow-md hover:bg-slate-500 hover:text-slate-100 border-2 border-slate-600"
         onClick={() => signIn('google')}
+        className="bg-slate-700 text-slate-200 font-bold text-3xl motion-safe:animate-pulse p-2 py-3 rounded-md shadow-md hover:bg-slate-500 hover:text-slate-100 border-2 border-slate-600"        
       >
         Sign In to use Gypsy
       </button>
