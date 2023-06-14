2a7c5798db3f6dadd557f84064a7c97c83aede20
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-06-14 13:34:41 -0500

    Chats in Sidebar are undefined??

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { useSession } from 'next-auth/react';
+import ChatMessage from './ChatMessage';
+import { collection, query, orderBy } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 type Props = {
     chatId: string;
 };
 
 const ChatWindow = ({ chatId }: Props) => {
+  const { data: session } = useSession();
+
+  const [messages] = useCollection(session && query(
+    collection(db, 'users', session?.user?.email!, 'chats', chatId, 'messages'),
+    orderBy('createdAt', 'asc')
+  ))
+
+
   return (
-    <div className='flex-1'>ChatWindow</div>
+    <div className='flex-1'>
+      {messages?.docs.map((message) => (
+        <ChatMessage key={message.id} message={message.data()} />
+      ))}
+    </div>
   );
 };
 
