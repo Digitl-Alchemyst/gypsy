52ee226ee3ad619089b8091eae0f3117360b9eea
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-06-14 13:46:48 -0500

    Messages Should Pull Error Instead

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import { DocumentData } from 'firebase/firestore'
 
@@ -8,7 +9,14 @@ type Props = {
 function ChatMessage({ message }: Props) {
 
   return (
-    <div>ChatMessage</div>
+    <div>
+        <div className='flex space-x-5 px-10 max-w-2xl mx-auto'>
+            <img src={message.user.avatar} alt='user' className='rounded-full h-8 w-8 cursor-pointer hover:opacity-50' />
+            <p className='pt-1 text-md'>
+                <span>{message.user}</span>
+            </p>
+        </div>
+    </div>
   );
 };
 
